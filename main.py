0b493c4d51a838c72d7bcd8d2a1680db45af8d87
SFAS0 <sfas7@mail.ru>
2023-11-06 23:15:51 +0300

    break

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pyautogui import moveTo, sleep, click
 from winsound import MessageBeep, MB_ICONHAND
 
 sleep(5)
-for i in range(25):
+for i in range(33):
     moveTo(1250, 275, 0.2)
     click(button="right")
     moveTo(1155, 275, 0.2)
@@ -13,6 +13,8 @@ for i in range(25):
     click(button="right")
     moveTo(835, 675, 0.2)
     click()
-    sleep(5)
+    if i == 32:
+        break
+    sleep(5.3)
 MessageBeep(MB_ICONHAND)
 # MessageBeep(-1)
